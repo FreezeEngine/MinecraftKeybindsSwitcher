3640a01ddd0b7f8823c33e400fcb086e65c8be2a
FreezeEngine <freezeengine@gmail.com>
2023-03-10 01:25:31 +0300

    Update

--- main.py
+++ main.py
@@ -1,38 +1,77 @@
-from ReadWriteMemory import ReadWriteMemory
+import json
+import os
+from typing import List
+import win32process
+from ReadWriteMemory import ReadWriteMemory, ReadWriteMemoryError
 from time import sleep
 import threading
 import tkinter as tk
-from tkinter import Button, Label, Entry
+from tkinter import Button, Label
 from tkinter import ttk
+from ctypes import *
+
+
+class Memory:
+    def __init__(self, process):
+        self.process = process
+
+    def read(self, address, buffer_size=4):
+        try:
+            buf = create_string_buffer(buffer_size)
+            bytes_read = c_ulong(0)
+            if windll.kernel32.ReadProcessMemory(self.process.handle, address, buf, buffer_size, byref(bytes_read)):
+                return buf
+            else:
+                print("Failed: Read Memory - Error Code: ", windll.kernel32.GetLastError())
+                return False
+        except (BufferError, ValueError, TypeError) as error:
+            print("Failed: Read Memory #2 - Error Code: ", windll.kernel32.GetLastError())
+            return False
+
+    def get_pointer(self, base_address: int, offsets: List[hex] = ()):
+        temp_address = c_uint64.from_buffer(self.read(c_uint64(base_address), 8)).value
+        pointer = 0x0
+        if not offsets:
+            return base_address
+        else:
+            for offset in offsets:
+                pointer = c_uint64(int(temp_address) + offset)
+                temp_address = c_uint64.from_buffer(self.read(pointer, 8)).value
+            return pointer
 
 
 class KeybindsChanger:
     def __init__(self, use_gui=True):
+        self.module_name = None
+        self.module_base = None
+        self.pointers_offset = None
 
-        base = 0x04000000
-        static_address_offset = 0x0491CA88
-        self.pointer_static_address = base + static_address_offset
         self.searching_for_process = False
         self.pointer_map = {}
 
         self.process = None
         self.use_gui = use_gui
-        if use_gui:
-            self.init_gui()
 
         self.profiles_list = {'values': []}
         self.status_label = {'text': ''}
 
+        self.memory = None
+
         self.start_process_search()
+
+        self.buttons = []
+
+        if use_gui:
+            self.init_gui()
 
     def init_gui(self):
         window = tk.Tk()
         window.title('Minecraft Keybinds Switcher')
-        window.geometry('330x500')
+        window.geometry('330x520')
         window.resizable(width=False, height=False)
 
-        self.status_label = Label(window, text='Searching for Minecraft')
-        self.status_label.grid(row=1, column=1, columnspan=2, ipadx=100, ipady=20, sticky='n')
+        self.status_label = Label(window, text='Searching for Minecraft', width=16)
+        self.status_label.grid(row=1, column=1, columnspan=2, ipadx=105, ipady=20, sticky='n')
 
         Label(window, text='Profile:').grid(row=2, column=1, pady=5, sticky='e')
 
@@ -41,56 +80,95 @@ class KeybindsChanger:
         self.profiles_list['values'] = ['Bridge', 'CTF', 'EggWars']
         self.profiles_list.grid(row=2, column=2, pady=5)
 
-        # Label(window, text='Name:').grid(row=3, column=1, sticky='e', pady=5)
-        # Entry(window, width=30).grid(row=3, column=2, pady=5)
-
         Button(window, text='SAVE CURRENT', width=38).grid(row=4, column=1, columnspan=2, pady=5)
         Button(window, text='DELETE SELECTED', width=38).grid(row=5, column=1, columnspan=2)
+        apply_btn_id = 0
+        for slot_id in range(9):
+            slot = Label(window, text=f'SLOT {slot_id + 1}: X', font=1)
+            slot.grid(row=6 + slot_id, column=1, columnspan=2, pady=5)
+            self.buttons.append(slot)
+            apply_btn_id = slot_id + 7
 
-        Label(window, text='SLOT 1: X', font=1).grid(row=6, column=1, columnspan=2, pady=5)
-        Label(window, text='SLOT 2: X', font=1).grid(row=7, column=1, columnspan=2, pady=5)
-        Label(window, text='SLOT 3: X', font=1).grid(row=8, column=1, columnspan=2, pady=5)
-        Label(window, text='SLOT 4: X', font=1).grid(row=9, column=1, columnspan=2, pady=5)
-        Label(window, text='SLOT 5: X', font=1).grid(row=10, column=1, columnspan=2, pady=5)
-        Label(window, text='SLOT 6: X', font=1).grid(row=11, column=1, columnspan=2, pady=5)
-        Label(window, text='SLOT 7: X', font=1).grid(row=12, column=1, columnspan=2, pady=5)
-        Label(window, text='SLOT 8: X', font=1).grid(row=12, column=1, columnspan=2, pady=5)
-        Label(window, text='SLOT 9: X', font=1).grid(row=13, column=1, columnspan=2, pady=5)
+        Button(window, text='APPLY', width=38).grid(row=apply_btn_id, column=1, columnspan=2, pady=14)
 
-        Button(window, text='APPLY', width=38).grid(row=14, column=1, columnspan=2, pady=14)
-        # frame = Frame(root, bg='#fafafa')
-        # frame.place(rely=0.10, relx=0.10, relheight=0.8, relwidth=0.8)
-        # button = Button(root, text='APPLY')
-        # button.pack(pady=10)
         window.mainloop()
 
     def start_process_search(self):
         if self.searching_for_process:
             return
         self.searching_for_process = True
-        threading.Thread(target=self.find_process, args=self.status_label).start()
-
-    def find_process(self, status_label):
-        rwm = ReadWriteMemory()
-        while True:
-            try:
-                process = rwm.get_process_by_name("Minecraft.Windows.exe")
-                process.open()
-                break
-            except:
-                sleep(3)
-                pass
-        self.searching_for_process = False
-        self.process = process
+        threading.Thread(target=self.load_pointer_map, daemon=True).start()
 
     def attach_and_load(self):
         pass
 
     def update_values(self):
-        pass
+        if not self.use_gui:
+            return
+        for slot_id in range(9):
+            slot = int(c_int32.from_buffer(self.memory.read(self.pointer_map[slot_id], 4)).value)
+            btn = f'MB{slot + 100}' if slot < 0 else chr(slot)
+            self.buttons[slot_id]['text'] = f'SLOT {slot_id + 1}: {btn}'
 
-    def load_pointer_map(self, filename):
-        self.pointer_map = {}
+    def realtime_values_update(self):
+        while True:
+            try:
+                self.update_values()
+                sleep(1)
+            except:
+                self.status_label['text'] = "Searching for Minecraft"
+                self.start_process_search()
+                break
+
+    def load_pointer_map(self, filename='memory_map_1.19.31.json'):
+        with open(f'memory_maps/{filename}') as file:
+            data = json.load(file)
+            base_struct = data['BaseAddress'].split('+')
+            self.module_name = base_struct[0]
+            # Find process
+            rwm = ReadWriteMemory()
+            while True:
+                try:
+                    process = rwm.get_process_by_name(self.module_name)
+                    process.open()
+                    break
+                except ReadWriteMemoryError:
+                    sleep(3)
+                    pass
+            self.status_label['text'] = "Attached to Minecraft"
+            self.searching_for_process = False
+            self.process = process
+            self.memory = Memory(process)
+            # Load modules
+            for handle in win32process.EnumProcessModules(self.process.handle):
+                module_base = handle  # base addr
+                module_name = os.path.basename(win32process.GetModuleFileNameEx(self.process.handle, handle))  # name
+                print({module_name: module_base})
+                if module_name == self.module_name:
+                    self.module_base = module_base
+                    break
+            if not self.module_base:
+                print('FAILED TO FIND MODULE')
+                return
+            # Calculate base
+            self.pointers_offset = int(base_struct[1], 16)
+            self.pointer_map = []
+            start_pointer_address = self.module_base + self.pointers_offset
+            # read pointers
+            slots = ['Slot1', 'Slot2', 'Slot3', 'Slot4', 'Slot5', 'Slot6', 'Slot7', 'Slot8', 'Slot9']
+            for slot in slots:
+                offsets_obj = []
+                offsets = data[slot].split(',')
+                for offset in offsets:
+                    offsets_obj.append(int(offset, 16))
+                    while True: # inf loop if mc version is diff
+                        try:
+                            pointer = self.memory.get_pointer(start_pointer_address, offsets=offsets_obj)
+                            break
+                        except:
+                            sleep(5)
+                self.pointer_map.append(pointer)
+            threading.Thread(target=self.realtime_values_update, daemon=True).start()
 
 
 if __name__ == '__main__':
